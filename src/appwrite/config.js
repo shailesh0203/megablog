a6c8e263a6df083d49286297f0e9442d7ae8ed55
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-04-11 19:23:08 +0530

    config

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,4 +1,4 @@
-import conf from '..conf.js'
+import conf from '../conf/conf.js'
 import { Client,ID,Databases,Storage,query } from 'appwrite'
 
 export class Service{
@@ -10,7 +10,61 @@ constructor(){
     this.client
      .setEndpoint(conf.appwriteUrl)
      .setProject(conf.appwriteProjectId)
-    this.account=new Account(this.client)
+    this.databases= new Databases(this.client);
+    this.bucket =new Storage(this.client);
+}
+
+async createPost({title,slug,content,featuredImage,status,userId}){
+    try {
+        return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            }
+            
+        )
+    } catch (error) {
+        console.log("Appwrite service::createPost::error",error);
+    }
+
+}
+
+async updatePost(slug,{title,content,featuredImage,status}){
+try {
+    return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                
+            }
+    )
+} catch (error) {
+    console.log("Appwrite service::createPost::error",error);
+}
+}
+async deletePost(slug){
+    try {
+        await this.deleteDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+        return true
+    } catch (error) {
+        console.log("Appwrite service::createPost::error",error);
+        return false;
+    }
 }
 }
 const service =new Service()
